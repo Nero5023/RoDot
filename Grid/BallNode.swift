50e2511b1e25129b1f590a5aca1b7eafd213b7a2
Nero5023 <502350123@qq.com>
2016-04-07 21:09:45 +0800

    Fix Rotaiton rest bug

--- Grid/BallNode.swift
+++ Grid/BallNode.swift
@@ -12,13 +12,14 @@ class BallNode: SKSpriteNode, CustomNodeEvents {
   
   func didMoveToScene() {
     physicsBody = SKPhysicsBody(circleOfRadius: size.width/2)
-    
+//    size = CGSize(width: 150, height: 150)
     physicsBody!.categoryBitMask = PhysicsCategory.Ball
     physicsBody!.collisionBitMask = PhysicsCategory.Rod | PhysicsCategory.PointNode | PhysicsCategory.Ball | PhysicsCategory.IceBall
     physicsBody!.contactTestBitMask = PhysicsCategory.Transfer | PhysicsCategory.Distance | PhysicsCategory.Edge
     physicsBody!.mass = 0.015
 //    physicsBody!.mass = 0.197511121630669
 //    physicsBody!.allowsRotation = false
+//    physicsBody!.usesPreciseCollisionDetection = true
     zPosition = 1000
   }
 }
